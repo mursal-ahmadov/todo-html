5fe94987f7e9544ce6b9095d850477fc5a4a7d3d
Mursal Ahmadov <mursal@todo-app>
2026-04-22 22:38:32 +0400

    Add planning features, reminders, lists and archive flow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'todo-app-v3';
-const ASSETS = ['./', './index.html', './manifest.json', './sw.js'];
+const CACHE_NAME = 'todo-app-v4';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js', './app.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
